cb7dff89eb6b5290e8df9d608e419b0c70a3889f
Riley Rohloff <rileyrohloff@workfront.com>
2020-03-18 19:27:03 -0600

    added hover state for card tiles and new hook to open and close cards

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CardWrapper, CardsContainer } from './styled';
 import GET_ALL_FILMS from '../../apollo/queries/getMovies';
 import { useQuery } from '@apollo/react-hooks';
 
 const Cards = ({sidebar}) => {
     const {loading, error, data} = useQuery(GET_ALL_FILMS);
+    const [ display, setDisplay ] = useState(true)
     if (loading) return <p>Loading...</p>;
     
     if (error) {
@@ -13,15 +14,26 @@ const Cards = ({sidebar}) => {
       return <p>Error... </p>
     }
     data.allFilms.films.sort((a, b) => a.episodeID - b.episodeID);
-    const filmData = data.allFilms.films.map(({title, episodeID}) => {
+    const filmData = data.allFilms.films.map(({title, episodeID, releaseDate, id}) => {
+
+      if (display !== true) {
        return(
         <CardsContainer key={episodeID}>
           <p>{title}</p>
           <p>{episodeID}</p>
+          <p>releaseDate</p>
+          <p>id</p>
         </CardsContainer>
        )
+      } else {
+        return (
+          <CardsContainer key={episodeID}>
+          <p>{title}</p>
+        </CardsContainer>
+        )
+      }
     });
-    return <CardWrapper sideBarOpen={sidebar}>{filmData}</CardWrapper>
+    return <CardWrapper onClick={() => setDisplay(!display)} sideBarOpen={sidebar}>{filmData}</CardWrapper>
 };
 
 export default Cards;
--- src/components/Cards/styled.js
+++ src/components/Cards/styled.js
@@ -8,11 +8,13 @@ export const CardWrapper = styled.div`
         margin-left: 45px;
     `}
     transition: margin-left .2s ease;
-
 `
 export const CardsContainer = styled.div`
     background-color: tomato;
     border-radius: 5px; 
     padding: 10px;
     margin: 10px;
+    :hover {
+        background-color: #FF5733;
+    }
     `

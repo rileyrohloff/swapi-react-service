0f8b2ee7ebe0daf6ba906415b657870bcc68d41b
Riley Rohloff <rileyrohloff@workfront.com>
2020-04-26 00:42:35 -0600

    started sidenav linking with styling

--- src/components/NavItems/styled.js
+++ src/components/NavItems/styled.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 export const NavTitle = styled.p`
+    display: flex;
+    justify-content: flex-start;
     font-weight: bold;
     color: red;
+    border: dotted;
+    border-color: black;
+    padding: 10px;
 `
 
